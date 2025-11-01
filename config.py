52e3171aba6ed3962bc2fde3d169ece8dfe183ae
romain isoz <isozromain@gmail.com>
2025-11-01 09:59:52 +0100

    add some config for new report

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 GENERATORS = {
-    'heading': 101,
+    'heading': 100,
+    'summary': 101,
     'spendings': 102,
-#     "report_generator.summary.summary.Summary": 101,
-#     "report_generator.spendings.spendings.Spendings": 102,
-#     "report_generator.top_month_spendings.top_month_spendings.TopMonthSpendings": 103,
+    'topMonthSpendings': 103
 }
